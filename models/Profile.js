193e1fad23a4d2c4cff63495237e0f0d337de400
Yifei Ren <yifeiren99@gmail.com>
2020-10-25 23:24:31 +0800

    log in API done, user profile model created

---
+++ models/Profile.js
@@ -0,0 +1,106 @@
+const mongoose = require('mongoose')
+
+const profileSchema = new mongoose.Schema({
+  user:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:'user' // the profile data is connected to user data
+  },
+  company:{
+    type: String
+  },
+  website:{
+    type: String
+  },
+  location:{
+    type: String
+  },
+  status:{
+    type: String,
+    required = true
+  },
+  skills:{
+    type: [String]
+  },
+  bio:{
+    type: String
+  },
+  githubusername:{
+    type: String
+  },
+  experience:[
+    {
+      title:{
+        type: String
+      },
+      company:{
+        type: String
+      },
+      location:{
+        type: String
+      },
+      from:{
+        type: Date,
+        required = true
+      },
+      to:{
+        type: Date,
+      },
+      current:{
+        type: Boolean,
+        default: false
+      },
+      description:{
+        type: String
+      }
+    }
+  ],
+  education:[
+    {
+      school:{
+        type: String,
+        required = true
+      },
+      degree:{
+        type: String
+      },
+      fieldofstudy:{
+        type: String
+      },
+      from:{
+        type: Date
+      },
+      to:{
+        type:Date
+      },
+      current:{
+        type: Boolean
+      },
+      description:{
+        type: String
+      }
+    }
+  ],
+  social:{
+    youtube:{
+      type: String
+    },
+    twitter:{
+      type: String
+    },
+    facebook:{
+      type: String
+    },
+    linkedin:{
+      type: String
+    },
+    instagram:{
+      type: String
+    }
+  },
+  date:{
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = Profile = mongoose.model('profile', profileSchema)
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,6 +3,10 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
 const User = require('../../models/Users')
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 
 // @route request type:GET, endpoint:api/auth
 // @description: Test route
@@ -12,7 +16,7 @@ const User = require('../../models/Users')
 // By adding this middleware, this route is now protected(only the user who has correct token can access it)
 router.get('/', auth, async(req, res) => { 
   try {
-    // find user's info(name, email, avatar...except password) in database according to req.user.id, and save them in "user"
+    // This function find user's info(name, email, avatar...except password) in database according to req.user.id, and save them in "user"
     const user = await User.findById(req.user.id).select('-password')
 
     // and send it back to client-side in json format (these info will be needed in frontend to render something)
@@ -22,5 +26,68 @@ router.get('/', auth, async(req, res) => {
     res.status(500).send('server error')
   } 
 })
+
+
+// @route request type: POST, endpoint:api/users
+// @desc: log in users. If the password is right, return a token, so the user can access protected route with his token
+// @access: public
+router.post('/',
+  [ // Use express-validator to check input
+    check('email', 'Please enter a valid email address')
+      .isEmail(),
+    check('password', 'Please enter a password')
+      .exists()
+  ],
+
+  async (req, res) => { 
+  const errors = validationResult(req);
+  // if there is an error in name/email/password
+  if (!errors.isEmpty()){
+    return res.status(400).json({ errors: errors.array() }) // 400:bad request
+  }
+
+  const { email, password } = req.body 
+
+  try{
+    // Make a query in MongoDB, to check if the user exists. If he exists, his document in DB will be saved in "user" object
+    let user = await User.findOne({email})
+    if (!user){
+      return res.status(400).json({errors: [{ msg: "User doesn't exist" }]})
+    }
+
+    // check if password is correct("password" is input from client side, "user.password" is the correct password in DB)
+    // "bcrypt.compare" compares a plain text password and a encrypted password(password in DB are encrypted)
+    //It returns a promise, so we add "await" in front of it 
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch){
+      res.status(400).json({errors: [{ msg:'incorrect password' }] })
+    }
+
+    // If the password is right, return jsonwebtoken(which contains user ID), so the user can access protected route with his token
+    const payload = {
+      user:{
+        id: user.id
+      }
+    }
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360011 }, // option
+      (err, token) => {
+        if(err) throw err;
+        res.json({ token }) // send token back to user, so he can access protected route with it later
+      }
+    )
+    
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('server error')
+  }
+  
+})
+
+
 
 module.exports = router
