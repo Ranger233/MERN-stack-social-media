a135e0df1ae44fefe7422917ec6788b35ed7abb1
Yifei Ren <yifeiren99@gmail.com>
2020-10-28 14:36:36 +0800

    add & remove comment routes done, the whole back-end done!

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -161,4 +161,78 @@ router.put('/unlike/:id', auth, async (req, res) => {
   }
 })
 
+
+// @route    POST   api/posts/comment/:id  (the id here is post's id)
+// @desc     comment on a post
+// @access   private
+router.post('/comment/:id', 
+  [
+    auth, 
+    check('text','text is required').not().isEmpty()
+  ],
+  async(req, res) => {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()){
+    res.status(400).json({ errors: errors.array()})
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+    const post = await Post.findById(req.params.id)
+
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id 
+    }
+
+    post.comments.unshift(newComment)
+
+    await post.save()
+    res.json(post.comments)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).json({ msg:'server error' })
+  }
+
+})
+
+
+// @route    DELETE   api/posts/comment/:post_id/:comment_id
+// @desc     delete a comment on a post
+// @access   private
+router.delete('/comment/:post_id/:comment_id', auth, async (req, res) =>{
+  try {
+    const post = await Post.findById(req.params.post_id)
+
+    // pull out comment
+    const comment = post.comments.find(item => item.id === req.params.comment_id)
+
+    // check if the comment exists
+    if(!comment){
+      return res.status(404).json({ msg:'comment not found' })
+    }
+
+    // if it's not the current user's comment, he can't delete it
+    if(req.user.id !== comment.user.toString()){
+      return res.status(401).json({ msg:'you can not delete others comment' })
+    }
+
+    // find index of comment to remove
+    const removeIndex = post.comments.map(item => item.id).indexOf(req.params.comment_id)
+
+    post.comments.splice(removeIndex, 1)
+
+    await post.save()
+
+    res.json(post.comments)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).json({ msg:'server error' })
+  }
+})
+
+
+
 module.exports = router
