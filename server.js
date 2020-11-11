bc0f9b99370876e6fdff3e26b000a906e133898e
Yifei Ren <yifeiren99@gmail.com>
2020-11-11 17:24:33 +0800

    ready to deploy to heroku

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 
-// Connect database, 这个函数是定义在config/db.js里的
+// Connect to database (defined in config/db.js)
 connectDB()
 
 // Init middleware. in order to have access to "req.body" from POST request
 app.use(express.json({ extended: false }))
-
-app.get('/', (req, res) => res.send('API running'))
 
 // Define routes
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/users', require('./routes/api/users'))
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 
 // process.env.PORT will look for an environment varible called PORT (used to deploy in Heroku), if it's not found, PORT=5000
 const PORT = process.env.PORT || 5000
